4ef2218439f45af29211215d1ca96848ea27f819
Lada <lada.astashenkova@yandex.ru>
2020-10-21 19:50:05 +0300

    Create a file uploader.

--- src/application/app_common_window.py
+++ src/application/app_common_window.py
@@ -1,3 +1,4 @@
+import os
 from collections import namedtuple
 
 from flask import Flask, render_template, redirect, url_for, request
@@ -9,10 +10,31 @@ Doc=namedtuple('Doc','text')
 docs=[]
 docs.clear()
 
-
 @app.route('/', methods=['GET', 'POST'])
 def main_page():
- return render_template('common_docs.html', docs=docs)
+    # return render_template('common_docs.html')
+    return render_template('common_docs.html', docs=docs)
+
+
+APP_ROOT = os.path.dirname(os.path.abspath(__file__))
+
+@app.route("/upload", methods=['POST'])
+def upload():
+    target = os.path.join(APP_ROOT, "files\\")
+    print(target)
+
+    if not os.path.isdir(target):
+        os.mkdir(target)
+
+    for file in request.files.getlist("file"):
+        print(file)
+        filename = file.filename
+        destination = "\\".join([target, filename])
+        print(destination)
+        file.save(destination)
+
+    # print("File upload")
+    return render_template('complete.html')
 
 
 @app.route('/add_text', methods=['POST'])
@@ -21,8 +43,6 @@ def add_text():
     docs.clear()
     docs.append(Doc(text))
     return redirect(url_for('main_page'))
-
-
 
 if __name__ == '__main__':
     app.run()
