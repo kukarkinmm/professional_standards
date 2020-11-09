121e094f45cb90f766905613a0b068f7193648a1
teachmehowtoprogram <teachmehowtoprogram@users.noreply.github.com>
2020-11-09 21:48:01 +0300

    fixes to xml parser

--- src/parsers/xml_parser.py
+++ src/parsers/xml_parser.py
@@ -1,4 +1,6 @@
 import xml.etree.ElementTree as et
+from os import listdir
+from os.path import isfile, join
 
 
 class XmlParser(object):
@@ -9,7 +11,7 @@ class XmlParser(object):
 
     def get_name(self):
         return self.root.find(".//NameProfessionalStandart").text
-
+    
     def get_relevant_text(self):
         return "\n".join({
             self.get_name(),
@@ -26,3 +28,31 @@ class XmlParser(object):
             "\n".join({el.text for el in self.root.findall(".//NecessaryKnowledge")}),
             "\n".join({el.text for el in self.root.findall(".//RequirementsWorkExperience")})
         })
+    
+    """
+    @staticmethod
+    def extract_text_from_xmls_recursively(dir):
+        '''
+        получение содержимого всех xml документов в директории
+        '''
+        all_texts = []
+        for root, dirs, files in os.walk(dir):
+            for file in files:
+                path_to_xml = os.path.join(root, file)
+                [stem, ext] = os.path.splitext(path_to_xml)
+                if ext == '.xml':
+                    print("Processing " + path_to_xml)
+                    XmlParser = XmlParser(path_to_xml)
+                    text = XmlParser.get_relevant_text()
+                    # xml_contents = XmlParser.get_relevant_text(path_to_xml)
+                    # text = xml_contents['content']
+                    all_texts.append(text)
+        return all_texts
+    """
+    
+    @staticmethod
+    def extract_text_from_xmls(path):
+        files = [f for f in listdir(path) if isfile(join(path, f))]
+        # profstandards = [XmlParser(f"{path}/{f}").get_relevant_text() for f in files]
+        profstandards = [XmlParser(join(path, f)).get_relevant_text() for f in files if f.endswith(".xml")]
+        return profstandards
